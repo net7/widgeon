c6a40646b9fd7d5ec727214828ef1200f4ce0146
gluca <guidi.luca@gmail.com>
2008-01-03 16:01:38 +0000

    [Widgeon] Fixing widgets generator.
    git-svn-id: http://svn.talia.discovery-project.eu/talia/widgeon/trunk@289 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- generators/widget/widget_generator.rb
+++ generators/widget/widget_generator.rb
@@ -1,7 +1,7 @@
 class WidgetGenerator < Rails::Generator::NamedBase
   def initialize(runtime_args, runtime_options = {})
     @widget_file_name = "#{runtime_args.first.underscore}_widget.rb"
-    @widget_view_file = "_#{runtime_args.first.underscore}_widget.rhtml"
+    @widget_view_file = "_#{runtime_args.first.underscore}_widget.html.erb"
     super
   end
   
@@ -10,12 +10,12 @@ class WidgetGenerator < Rails::Generator::NamedBase
       # Check for class collisions
       m.class_collisions class_name
       
-      m.directory File.join(Widgeon::Widget.widgets_folder, name.underscore)
+      m.directory File.join(Widgeon::Widget.path_to_widgets, name.underscore)
       
       # Create files
-      m.template 'widget.rb', File.join(Widgeon::Widget.widgets_folder, @widget_file_name),
+      m.template 'widget.rb', File.join(Widgeon::Widget.path_to_widgets, name.underscore, @widget_file_name),
         :assigns => { :widget_class_name => class_name }
-      m.template 'widget.rhtml', File.join(Widgeon::Widget.widgets_folder, name.underscore, @widget_view_file),
+      m.template 'widget.rhtml', File.join(Widgeon::Widget.path_to_widgets, name.underscore, @widget_view_file),
         :assigns => { :widget_name => name.underscore }
     end
   end
