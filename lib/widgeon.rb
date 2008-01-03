7464d20098fc8c31407185a779f11b7803335c44
gluca <guidi.luca@gmail.com>
2008-01-03 14:31:21 +0000

    [Widgeon] Implementing loading mechanism.
    git-svn-id: http://svn.talia.discovery-project.eu/talia/widgeon/trunk@284 2960d7dc-a92c-0410-a2fe-c68badc242c0

--- lib/widgeon.rb
+++ lib/widgeon.rb
@@ -1,11 +1,23 @@
 # Widgeon
 module Widgeon
   class Widget
-    PATH_TO_WIDGETS = 'app/views/widgets'
-    
     class << self
       def path_to_widgets
-        PATH_TO_WIDGETS
+        @@path_to_widgets ||= 'app/views/widgets'
+      end
+      
+      # Attempts to load the widget with the given name. The behaviour depends
+      # on Rails::Configuration.cache_classes: If that is set to false, the
+      # file will always be reloaded. If true, the widget class will be
+      # loaded only once.
+      def load(widget_name)
+        raise(ArgumentError, "Unable to load widget: " + widget_name) unless exists?(widget_name)
+        require_or_load File.join(path_to_widgets, widget_name, widget_name+'_widget')
+      end
+      
+      # Check if a widget exists in the path defined in path_to_widgets.
+      def exists?(widget_name)
+        File.exists?(path_to_widgets+'/'+widget_name.to_s)
       end
     end
   end
--- test/widgeon_test.rb
+++ test/widgeon_test.rb
@@ -5,7 +5,34 @@ require 'vendor/plugins/widgeon/test/test_helper'
 class WidgeonTest < Test::Unit::TestCase
   include Widgeon
 
+  def setup
+    @path_to_widgets = "app/views/widgets"
+  end
+
+  def teardown
+    Widget.send(:class_variable_set, :@@path_to_widgets, @path_to_widgets)
+  end
+
   def test_widget_paths
-    assert_equal("app/views/widgets", Widget.path_to_widgets)
+    assert_equal(@path_to_widgets, Widget.path_to_widgets)
+  end
+    
+  def test_exists
+    set_fixtures_folder
+    assert !Widget.exists?(:unexistent)
+    assert  Widget.exists?(:hello_world)
+  end
+  
+  def test_load
+    set_fixtures_folder
+    assert_raise(ArgumentError) { Widget.load("unexistent") }
+
+    Widget.load("hello_world")
+    assert defined? HelloWorldWidget
+  end
+  
+  private
+  def set_fixtures_folder
+    Widget.send(:class_variable_set, :@@path_to_widgets, File.join(File.dirname(__FILE__), 'fixtures'))
   end
 end
